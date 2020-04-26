5193bc48325fb070ba1e2dc2ec698b9c7ad0ce76
KentPhan <kent.chi.lam@gmail.com>
2020-04-25 22:34:10 -0600

    More Cloth Code

--- ClothSimulation/Assets/Scripts/KentClothSim.cs
+++ ClothSimulation/Assets/Scripts/KentClothSim.cs
@@ -8,7 +8,7 @@ namespace Assets
     {
         public Vector3 Position;
         public Vector3 Velocity;
-        public Vector3 Forces;
+        public Vector3 Force;
         //List<ClothVertex> Neighbors;
     }
 
@@ -16,10 +16,14 @@ namespace Assets
     public class KentClothSim : MonoBehaviour
     {
         [Header("Cloth Properties")]
-        [SerializeField] private float SpringStiffness;
-        [SerializeField] private float PointMass;
-        [SerializeField] private Vector3 GravityConstant = new Vector3(0.0f,-9.8f,0.0f);
-        
+        [SerializeField] [Range(0,20)] private float SPRING_STIFFNESS_CONSTANT = 1.0f;
+        [SerializeField] [Range(0, 20)] private float VERTEX_MASS = 1.0f;
+        [SerializeField] private Vector3 GRAVITY_CONSTANT = new Vector3(0.0f,-9.8f,0.0f);
+
+
+        [Header("Spring Properties")]
+        [SerializeField] [Range(0, 20)] private float SPRING_REST_LENGTH = 0.5f;
+
 
         private Dictionary<Vector2Int, ClothVertex> clothVertexStructure;
         private Vector2Int clothResolution = new Vector2Int(11,11);// Hardcoded due to used plane mesh
@@ -67,6 +71,28 @@ namespace Assets
             return toReturn;
         }
 
+        private static List<ClothVertex> GetNeighbors(Dictionary<Vector2Int, ClothVertex> clothStructure,
+            Vector2Int targetKey)
+        {
+            List<ClothVertex> neighbors = new List<ClothVertex>();
+
+            // Loop through neighbors
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0)
+                        continue;
+                    Vector2Int offset = new Vector2Int(i, j);
+                    if (clothStructure.ContainsKey(targetKey + offset))
+                    {
+                        neighbors.Add(clothStructure[targetKey + offset]);
+                    }
+                }
+            }
+            return neighbors;
+        }
+
 
         private static Vector3[] ConstructVertexArrayFromDataStructure(Dictionary<Vector2Int, ClothVertex> clothStructure, Vector2Int resolution)
         {
@@ -103,15 +129,15 @@ namespace Assets
             //}
 
 
-            UpdateSimulateCloth(this.clothVertexStructure);
-
-            GetComponent<MeshFilter>().mesh.vertices = ConstructVertexArrayFromDataStructure(this.clothVertexStructure,this.clothResolution);
+            
         }
 
 
         private void FixedUpdate()
         {
-            
+            UpdateSimulateCloth(this.clothVertexStructure);
+
+            GetComponent<MeshFilter>().mesh.vertices = ConstructVertexArrayFromDataStructure(this.clothVertexStructure, this.clothResolution);
         }
 
 
@@ -119,16 +145,65 @@ namespace Assets
 
         private void UpdateSimulateCloth(Dictionary<Vector2Int, ClothVertex> clothStructure)
         {
+
+            float deltaTime = Time.deltaTime;
+
             foreach (KeyValuePair<Vector2Int, ClothVertex> keyPair in clothStructure)
             {
 
-                if (keyPair.Key.x % 2 == 0)
+                Vector2Int key = keyPair.Key;
+                ClothVertex currentClothVertex = keyPair.Value;
+
+                // Calculate Forces
                 {
-                    //keyPair.Value.Position.x = Mathf.Sin(Time.time);
-                    //vertices[i] += normals[i] * Mathf.Sin(Time.time);
+                    //Gravity
+                    currentClothVertex.Force = GRAVITY_CONSTANT * VERTEX_MASS;
+
+
+                    // TODO Wind
+
+                    // TODO Air Resistance
+
+
+
+
+                    //Spring Forces
+                    List<ClothVertex> neighbors = GetNeighbors(clothStructure, key);
+                    foreach (ClothVertex neighbor in neighbors)
+                    {
+                        Vector3 springForceVectorToCenter = (currentClothVertex.Position - neighbor.Position  );
+                        Vector3 springForceDirectionToCenter = springForceVectorToCenter.normalized;
+
+
+                        // Longer = Positive = Force Should Go Toward Current Cloth Vertex
+                        // Shorter = Negative = Force Should Go Toward Neighbor
+                        float force = this.SPRING_STIFFNESS_CONSTANT * (springForceVectorToCenter.magnitude - this.SPRING_REST_LENGTH);
+                        
+
+                        currentClothVertex.Force += springForceDirectionToCenter * force;
+                    }
                 }
 
 
+
+
+                // Integrate Velocity
+                {
+                    currentClothVertex.Velocity = currentClothVertex.Velocity +
+                                                  ((currentClothVertex.Force / VERTEX_MASS) * (deltaTime));
+                }
+
+                // Integrate Position
+                {
+                    currentClothVertex.Position = currentClothVertex.Position +
+                                                  ((currentClothVertex.Velocity) * (deltaTime));
+                }
+
+                //if (keyPair.Key.x % 2 == 0)
+                //{
+                //    //keyPair.Value.Position.x = Mathf.Sin(Time.time);
+                //    //vertices[i] += normals[i] * Mathf.Sin(Time.time);
+                //}
             }
         }
 
@@ -136,15 +211,48 @@ namespace Assets
 
         private void OnDrawGizmos()
         {
-            
-            Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
-            for ( int i = 0 ; i < mesh.vertices.Length; i++)
+
+            if (this.clothVertexStructure != null)
             {
-                Vector3 vertex = mesh.vertices[i];
+                Matrix4x4 transform = this.transform.localToWorldMatrix;
 
-                vertex = this.transform.localToWorldMatrix.MultiplyPoint(vertex);
+                foreach (KeyValuePair<Vector2Int, ClothVertex> keyPair in this.clothVertexStructure)
+                {
 
-                Gizmos.DrawSphere(vertex, 0.1f);
+
+
+
+
+                }
+
+
+
+                Vector2Int key = new  Vector2Int(0,0);
+                var neighbors = GetNeighbors(this.clothVertexStructure, key);
+                var centerVertex = this.clothVertexStructure[key];
+
+                Gizmos.color = Color.yellow;
+                
+                Gizmos.DrawSphere(transform.MultiplyPoint(centerVertex.Position), 0.1f);
+
+                Gizmos.color = Color.red;
+                foreach (var neighbor in neighbors)
+                {
+                    Gizmos.DrawSphere(transform.MultiplyPoint(neighbor.Position), 0.1f);
+                }
+            }
+
+            else
+            {
+                Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+                for (int i = 0; i < mesh.vertices.Length; i++)
+                {
+                    Vector3 vertex = mesh.vertices[i];
+
+                    vertex = this.transform.localToWorldMatrix.MultiplyPoint(vertex);
+
+                    Gizmos.DrawSphere(vertex, 0.1f);
+                }
             }
         }
     }
